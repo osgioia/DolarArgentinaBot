3a199a683a71ac1d0e2cdc1defe86afc1dac50c0
Osvaldo Gioia <osvaldogioia@hotmail.com>
2020-03-20 19:03:19 -0300

    Update cron.py

--- cron.py
+++ cron.py
@@ -5,7 +5,7 @@ import pytz
 weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
 tz_AR = pytz.timezone('America/Argentina/Buenos_Aires') 
 datetime_AR = datetime.datetime.now(tz_AR)
-if datetime_AR.weekday() < 4:
+if datetime_AR.weekday() <= 4:
     if datetime_AR.hour >= 10 and datetime_AR.hour <= 16: # Horario Bancario
         exec(open("main.py").read())
 
